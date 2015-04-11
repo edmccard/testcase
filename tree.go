3f79358564172909fe3cce737c3f5706cefd2451
Ed McCardell <edmccard@hotmail.com>
2015-04-11 15:36:48 -0400

    Move exp from struct to parameter

--- tree.go
+++ tree.go
@@ -7,31 +7,34 @@ type Testable interface {
 	Diff(other Testable) interface{}
 }
 
-type Branch func(Tree, Testable)
+type Branch func(Tree, Testable, Testable)
 
 type Tree struct {
 	Name     string
 	T        *testing.T
 	Branches []Branch
-	Exp      Testable
 }
 
-func (t Tree) Run(name string, init Testable) bool {
+func (t Tree) Run(name string, init, exp Testable) bool {
 	if len(t.Branches) == 0 {
-		if !t.Exp.Equals(init) {
-			diff := t.Exp.Diff(init)
+		if !exp.Equals(init) {
+			diff := exp.Diff(init)
 			if diff != nil {
 				t.T.Errorf("%s %s: %s", t.Name, name, diff)
 			}
 			return false
 		}
 	} else {
-		next := Tree{t.Name + " " + name, t.T, t.Branches[1:], t.Exp}
-		t.Branches[0](next, init)
+		next := Tree{t.Name + " " + name, t.T, t.Branches[1:]}
+		t.Branches[0](next, init, exp)
 	}
 	return true
 }
 
+func (t Tree) Start(init Testable) bool {
+	return t.Run("", init, nil)
+}
+
 func NewTree(t *testing.T, name string, branches ...Branch) Tree {
-	return Tree{name, t, branches, nil}
+	return Tree{name, t, branches}
 }
